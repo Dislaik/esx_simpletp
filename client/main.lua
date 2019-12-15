d4caabe1d2a347819b0f5700bca706180ffefc17
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2019-12-15 11:10:17 -0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -7,42 +7,38 @@ Citizen.CreateThread(function()
 	end
 end)
 
-DistanceView = 10.0
-DistanceActivate = 1.0
-
 Citizen.CreateThread(function()																							
 	while true do
 		Citizen.Wait(1)
 		player = GetPlayerPed(-1)
-		coords = GetEntityCoords(player)		
+		coords = GetEntityCoords(player)
 		
-		---[Enter]
-		if GetDistanceBetweenCoords(coords, 289.15, -1094.96, 29.42, true) < DistanceView then --Distance established where you can see the text
-			ESX.Game.Utils.DrawText3D(vector3(287.75, -1095.07, 29.82), "~g~[REFUGIO]\n~c~Presiona ~g~[E] ~c~para entrar", 1.2, 4)
-			if GetDistanceBetweenCoords(coords, 288.62, -1095.13, 29.42, true) < DistanceActivate then --Distance you can interact
-				if not IsPedInAnyVehicle(player, false) then
-					if IsControlJustPressed(0, 38) then --Press [E]
-						DoScreenFadeOut(1000)
-						Citizen.Wait(1500)
-						SetEntityHeading(player, 357.43) --Direction in which the player will be looking
-						ESX.Game.Teleport(player, { x = 265.16, y = -1002.65, z = -100.01 }) --Site where the player will appear
-						DoScreenFadeIn(1500)
-					end
-				end
-			end
-		end
-		
-		--[Exit]
-		if GetDistanceBetweenCoords(coords, 266.06, -1007.6, -101.01, true) < DistanceView then --Distance established where you can see the text
-			ESX.Game.Utils.DrawText3D(vector3(266.06, -1007.9, -100.71), "~g~[REFUGIO]\n~c~Presiona ~g~[E] ~c~para salir", 1.2, 4)
-			if GetDistanceBetweenCoords(coords, 266.06, -1007.6, -101.01, true) < DistanceActivate then --Distance you can interact
-				if not IsPedInAnyVehicle(player, false) then
-					if IsControlJustPressed(0, 38) then --Press [E]
-						DoScreenFadeOut(1000)
-						Citizen.Wait(1500)
-						SetEntityHeading(player, 266.92) ----Direction in which the player will be looking
-						ESX.Game.Teleport(player, { x = 290.72, y = -1094.86, z = 28.41 }) --Site where the player will appear
-						DoScreenFadeIn(1500)
+		for k, v in pairs(Config.Teleport) do
+			if GetDistanceBetweenCoords(coords, v.Pos.x, v.Pos.y, v.Pos.z, true) < v.View then 
+				ESX.Game.Utils.DrawText3D(vector3(v.Pos.x, v.Pos.y, v.Pos.z), v.Text, 1.2, 4)
+				if GetDistanceBetweenCoords(coords, v.Pos.x, v.Pos.y, v.Pos.z, true) < v.Activate then
+					if not IsPedInAnyVehicle(player, false) then
+						if IsControlJustPressed(0, 38) then
+							if Config.Animation then
+								RequestAnimDict("anim@mp_atm@enter")
+								while not HasAnimDictLoaded("anim@mp_atm@enter") do
+									Citizen.Wait(1000)
+								end
+									
+								Citizen.Wait(200)
+								TaskPlayAnim(player,"anim@mp_atm@enter","enter",1.0, 1.0, 3000, 9, 1.0, 0, 0, 0)
+								Citizen.Wait(3000)
+							end
+							if Config.Blackout then
+								DoScreenFadeOut(1000)
+								Citizen.Wait(1500)
+							end
+							SetEntityHeading(player, v.Heading)
+							ESX.Game.Teleport(player, v.Posout) 
+							if Config.Blackout then
+								DoScreenFadeIn(1500)
+							end
+						end
 					end
 				end
 			end
