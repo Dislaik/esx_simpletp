42a56fe5b5e08a1bee723a908a2d3feb7f72d4ac
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2019-12-15 11:11:09 -0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,24 @@
+Config = {}
+Config.Blackout = true
+Config.Animation = true
+
+Config.Teleport = {
+
+	Enter = {
+		Pos = {x = 287.75, y = -1095.07, z = 29.82},
+		Heading = 357.43,
+		View = 10.0,
+		Activate = 1.0,
+		Text = '~g~[Departament]\n~s~Press [E] to enter',
+		Posout = {x = 265.16, y = -1002.65, z = -100.01}
+	},
+	
+	Exit = {
+		Pos = {x = 266.06, y = -1007.9, z = -100.71},
+		Heading = 266.92,
+		View = 5.0,
+		Activate = 1.0,
+		Text = '~g~[Departament]\n~s~Press [E] to exit',
+		Posout = {x = 290.72, y = -1094.86, z = 28.41}
+  }
+}
