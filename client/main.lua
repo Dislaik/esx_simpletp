65276f1c39f138593dfd04a8d57722f26a39aba0
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2019-12-15 05:48:03 -0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -20,12 +20,14 @@ Citizen.CreateThread(function()
 		if GetDistanceBetweenCoords(coords, 289.15, -1094.96, 29.42, true) < DistanceView then --Distance established where you can see the text
 			ESX.Game.Utils.DrawText3D(vector3(287.75, -1095.07, 29.82), "~g~[REFUGIO]\n~c~Presiona ~g~[E] ~c~para entrar", 1.2, 4)
 			if GetDistanceBetweenCoords(coords, 288.62, -1095.13, 29.42, true) < DistanceActivate then --Distance you can interact
-				if IsControlJustPressed(0, 38) then --Press [E]
-					DoScreenFadeOut(1000)
-					Citizen.Wait(1500)
-					SetEntityHeading(player, 357.43) --Direction in which the player will be looking
-					ESX.Game.Teleport(player, { x = 265.16, y = -1002.65, z = -100.01 }) --Site where the player will appear
-					DoScreenFadeIn(1500)
+				if not IsPedInAnyVehicle(player, false) then
+					if IsControlJustPressed(0, 38) then --Press [E]
+						DoScreenFadeOut(1000)
+						Citizen.Wait(1500)
+						SetEntityHeading(player, 357.43) --Direction in which the player will be looking
+						ESX.Game.Teleport(player, { x = 265.16, y = -1002.65, z = -100.01 }) --Site where the player will appear
+						DoScreenFadeIn(1500)
+					end
 				end
 			end
 		end
@@ -34,12 +36,14 @@ Citizen.CreateThread(function()
 		if GetDistanceBetweenCoords(coords, 266.06, -1007.6, -101.01, true) < DistanceView then --Distance established where you can see the text
 			ESX.Game.Utils.DrawText3D(vector3(266.06, -1007.9, -100.71), "~g~[REFUGIO]\n~c~Presiona ~g~[E] ~c~para salir", 1.2, 4)
 			if GetDistanceBetweenCoords(coords, 266.06, -1007.6, -101.01, true) < DistanceActivate then --Distance you can interact
-				if IsControlJustPressed(0, 38) then --Press [E]
-					DoScreenFadeOut(1000)
-					Citizen.Wait(1500)
-					SetEntityHeading(player, 266.92) ----Direction in which the player will be looking
-					ESX.Game.Teleport(player, { x = 290.72, y = -1094.86, z = 28.41 }) --Site where the player will appear
-					DoScreenFadeIn(1500)
+				if not IsPedInAnyVehicle(player, false) then
+					if IsControlJustPressed(0, 38) then --Press [E]
+						DoScreenFadeOut(1000)
+						Citizen.Wait(1500)
+						SetEntityHeading(player, 266.92) ----Direction in which the player will be looking
+						ESX.Game.Teleport(player, { x = 290.72, y = -1094.86, z = 28.41 }) --Site where the player will appear
+						DoScreenFadeIn(1500)
+					end
 				end
 			end
 		end
